bd44d24cce9ec5f09f9c8552cd906930523717a2
Laurenne <laurenneoliver@gmail.com>
2024-10-27 20:04:19 -0500

    added test scripts

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders CS3345 heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/CS3345/i);
+  expect(headingElement).toBeInTheDocument();
 });
